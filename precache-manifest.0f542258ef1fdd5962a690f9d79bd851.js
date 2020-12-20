621e8b2d141d8b804bdd90fa97a5cc28b72800ec
Fausto Morales <faustomorales@gmail.com>
2020-12-19 20:35:57 -0600

    Updates

---
+++ precache-manifest.0f542258ef1fdd5962a690f9d79bd851.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec8b668ea328026cb7f7c183cdf49e36",
+    "url": "/retinanetjs-example-app/index.html"
+  },
+  {
+    "revision": "be5379be2ab5e39328fe",
+    "url": "/retinanetjs-example-app/static/css/main.5863790c.chunk.css"
+  },
+  {
+    "revision": "600202353ce1864f05b6",
+    "url": "/retinanetjs-example-app/static/js/2.37feaf27.chunk.js"
+  },
+  {
+    "revision": "be5379be2ab5e39328fe",
+    "url": "/retinanetjs-example-app/static/js/main.5f242289.chunk.js"
+  },
+  {
+    "revision": "7dcceb14c0ef990927e8",
+    "url": "/retinanetjs-example-app/static/js/runtime~main.1ac5b0a7.js"
+  }
+]);
